73c724747b7a1d1c838e007ffb01c1a5d09ce12f
jaime jimenez <ja.jimenezchl@gmail.com>
2024-01-15 22:08:10 -0300

    primer commit

--- src/app/ui/card.jsx
+++ src/app/ui/card.jsx
@@ -4,36 +4,35 @@ import { useState , useEffect } from "react";
 import { randomNumber } from "@/app/utils";
 import { array_letters } from "@/app/definitions";
 
+var list_used = [];
 
-export default function table(props) {
+export default function table() {
 
     
-    const [list_letter, setlist_letter] = useState(2);   
-    get_letter_enable(list_letter)
+    // const [list_letter, setlist_letter] = useState(2);   
+    // get_letter_enable(list_letter)
 
     return (
         <>
-        < Card />
-        < Card />
-        < Card />
-        < Card />
-        < Card />
-        < Card />
+        <Card letter_active={ get_letter_enable()}/>
+        <Card letter_active={ get_letter_enable()}/>
+        <Card letter_active={ get_letter_enable()}/>
+        <Card letter_active={ get_letter_enable()}/>
+        <Card letter_active={ get_letter_enable()}/>
+        <Card letter_active={ get_letter_enable()}/>
         </>
     )
 }
 
 export function Card(props) {
 
-    const [Letter, setLetter] = useState();
+    const [Letter, setLetter] = useState(props.letter_active);
 
     const handleClick = () => {
-        setLetter(randomNumber(0, 25));
+        setLetter(get_letter_enable());
     }
 
-    // useEffect(() => {
-    //     randomNumber(0, 25)
-    // })
+
 
     return (
         <div className="flex justify-center items-center bg-gray-100 w-40 min-h-40 border-2 shadow-lg p-4 rounded m-4" onClick={handleClick} >
@@ -46,6 +45,11 @@ export function Card(props) {
 
 
 
-function get_letter_enable(letter){
-console.log(letter)
+function get_letter_enable(){
+ let letter_rm = randomNumber(0, 25);
+ if (!list_used.includes(letter_rm)){
+    list_used.push(letter_rm);
+    console.log(list_used)
+    return letter_rm;
+ }
 }
